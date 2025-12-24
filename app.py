c187eda4b6c617064a3a71262ea724fb4eeb1ca8
Filip <fwilk79@gmail.com>
2025-12-24 15:52:12 +0100

    Intitial Commit

---
+++ app.py
@@ -0,0 +1,8 @@
+from PIL import Image
+import streamlit as st
+
+camera_image=st.camera_input("Camera")
+if camera_image:
+    img = Image.open(camera_image)
+    gray_img=img.convert('L')
+    st.image(gray_img)
